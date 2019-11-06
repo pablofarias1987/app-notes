24ac82645c95cb84611d6182a004e542a8a082ca
pablo <pfgallardo1987@gmail.com>
2019-11-07 00:28:32 +0100

    login

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+//encripta password
+
+UserSchema.methods.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  };
+  
+  //compara los dos password ingresados
+  UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  
+  module.exports = mongoose.model('User', UserSchema);
+  
